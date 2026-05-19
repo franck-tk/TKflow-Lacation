8ffdc6064ba15b1a05fbb07ff87b5791f97fd5fa
franck-tk <todjoumfranck@.com>
2026-05-19 01:10:25 +0100

    fix railway deployment

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const nodemailer = require('nodemailer');
 const { v4: uuidv4 } = require('uuid');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
